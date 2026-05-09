a17b242d3dc0b468ad353ddde01b10840575296c
Harshittiwari73 <112146773+Harshittiwari73@users.noreply.github.com>
2026-05-09 23:35:06 +0530

    new changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,8 +10,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5021',
-        changeOrigin: true
+        target: 'https://ballsortgameproject.onrender.com',
+        changeOrigin: true,
+        secure: true
       }
     }
   }
